9ecd8fde70a7d1fd239ed31426ad6f7599f9ab22
Aditya <adityagodofgamer.1@gmail.com>
2020-08-10 20:49:21 +0530

    Adding Scroll Feature

---
+++ index.js
@@ -0,0 +1,15 @@
+var btnone =document.querySelector('main section a');
+btnone.addEventListener('click',function(event){
+    event.preventDefault();
+    var content=document.getElementById('content');
+    
+    var interval =setInterval(function(){
+        var coordinates =content.getBoundingClientRect();
+        if(coordinates.top<= 0){
+            clearInterval(interval);
+            return;
+        }
+        window.scrollBy(0,50);
+    }, 50);
+
+});
